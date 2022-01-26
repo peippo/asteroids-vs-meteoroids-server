e2de0437ffe8be2a6e4a0effcee57b7ae74cd4df
Janne Peippo <janne.peippo@poutapilvi.fi>
2022-01-26 21:42:46 +0200

    Save game info & remove on disconnect

--- index.js
+++ index.js
@@ -12,30 +12,56 @@ const io = socketIo(server, {
 	},
 });
 
+const games = {};
+
 io.on("connection", (socket) => {
 	console.log("Client connected");
 
 	// Create new game
 	socket.on("createNewGame", () => {
-		const gameId = Math.floor(1000 + Math.random() * 9000).toString();
-		socket.emit("newGameCreated", gameId);
+		const gameId = Math.floor(10000 + Math.random() * 90000).toString();
+		socket.emit("newGameCreated", { gameId: gameId, userId: socket.id });
 		socket.join(gameId);
+		games[gameId] = { hostId: socket.id };
+
+		console.log(`ID ${socket.id} created game ${gameId}`);
 	});
 
-	// Join a game
+	// Client trying to join a game
 	socket.on("joinGame", (gameId) => {
-		socket.join(gameId);
-		socket.to(gameId).emit("joinedGame");
+		if (games[gameId]) {
+			socket.join(gameId);
+			socket.emit("joinedGame", { gameId: gameId, userId: socket.id });
+			socket.to(gameId).emit("clientReady");
+			games[gameId]["clientId"] = socket.id;
+
+			console.log(`ID ${socket.id} joining game ${gameId}`);
+		} else {
+			socket.emit("gameNotFound");
+		}
 	});
 
 	// Start the game
 	socket.on("startGame", (gameId) => {
 		socket.to(gameId).emit("hostReady");
+
+		console.log(`Host ready to start game ${gameId}`);
 	});
 
 	// Disconnect
 	socket.on("disconnect", () => {
-		console.log("Client disconnected");
+		console.log(`Client ${socket.id} disconnected`);
+
+		// Remove game when the host or client disconnects
+		const gameToEnd = Object.keys(games).find(
+			(gameId) =>
+				games[gameId].hostId === socket.id ||
+				games[gameId].clientId === socket.id
+		);
+
+		if (gameToEnd) {
+			delete games[gameToEnd];
+		}
 	});
 });
 
